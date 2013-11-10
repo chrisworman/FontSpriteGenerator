16e512ccb3091db30ef2e6915d48c3e992bc89be
chrisworman <chrisworman@gmail.com>
2013-11-10 11:04:43 -0600

    Added a comment for commit testing

--- MainWindowController.cs
+++ MainWindowController.cs
@@ -11,6 +11,9 @@ namespace FontSpriteGenerator
 	public partial class MainWindowController : MonoMac.AppKit.NSWindowController
 	{
 
+		/// <summary>
+		/// The set of supported characters separated by spaces.
+		/// </summary>
 		public const string Characters = " . , : ; \" ' ! ? - ( ) # @ $ % & * + | / 0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z  ";
 		//public const string Characters = ".,:;\"'!?-()#@$%&*+|/0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
